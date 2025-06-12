60c34a4039a65536a7905478594f8045c2a74eff
MughniRayhan <tishtisha13@gmail.com>
2025-06-13 03:52:43 +0600

    add delete functionality for food items

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
       res.send(result);
     })
 
+     app.delete('/foods/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
+
        app.post('/foods', async (req, res) => {
       const newFood = req.body;
       const result = await foodCollection.insertOne(newFood);
